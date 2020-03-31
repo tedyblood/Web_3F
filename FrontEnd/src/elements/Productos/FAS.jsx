e05ff8114f04bde0cac7141bec263086be9b99a1
tedyblood <tedy.panama@gmail.com>
2020-03-30 22:52:12 -0500

    Imagenes de FAS Pendientes

--- FrontEnd/src/elements/Productos/FAS.jsx
+++ FrontEnd/src/elements/Productos/FAS.jsx
@@ -1,28 +1,48 @@
 const FAS = [
   {
-    images: "4-100",
+    images: "0",
     title: "Ultra",
     category: "FAS"
   },
   {
-    images: "4-100",
+    images: "1-100",
     title: "Ultra Diesel",
     category: "FAS"
   },
 
   {
-    images: "1-100",
-    title: "Mini",
-    category: "FAS"
-  },
-  {
-    images: "0",
+    images: "3-100",
     title: "Phoenix",
     category: "FAS"
   },
   {
-    images: "0",
+    images: "4-100",
+    title: "Mini",
+    category: "FAS"
+  },
+  {
+    images: "5-100",
+    title: "Fly",
+    category: "FAS"
+  },
+  {
+    images: "6-100",
     title: "Fire Angel",
+    category: "FAS"
+  },
+  {
+    images: "7-100",
+    title: "Fire Flash",
+    category: "FAS"
+  },
+  {
+    images: "8-100",
+    title: "FASMAX",
+    category: "FAS"
+  },
+  {
+    images: "9-100",
+    title: "MX10",
     category: "FAS"
   }
 ];
